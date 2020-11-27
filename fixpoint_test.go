df75f98defefb713634fdd7c91b0fc5e41a5b3ad
ezmicken <smoggach@gmail.com>
2020-11-26 20:40:24 -0500

    reduce accuracy of rotation test, update type names in test.

--- fixpoint_test.go
+++ fixpoint_test.go
@@ -7,29 +7,29 @@ import (
   "github.com/stretchr/testify/assert"
 )
 
-func TestQ6(t *testing.T) {
-  two := Q6FromFloat(2)
+func TestQ16(t *testing.T) {
+  two := Q16FromFloat(2)
   for _, f := range []float32{0.25, 1, 10, 0.125} {
-    q := Q6FromFloat(f)
+    q := Q16FromFloat(f)
     assert.Equal(t, f, q.Float(), "float32 roundtrip failed")
-    assert.Equal(t, Q6FromFloat(f*2), q.Mul(two), "multiply by 2")
+    assert.Equal(t, Q16FromFloat(f*2), q.Mul(two), "multiply by 2")
     square := q.Mul(q)
-    assert.Equal(t, Q6FromFloat(f*f), square, "square")
-    assert.Equal(t, Q6FromFloat(f), square.Div(q), "div")
+    assert.Equal(t, Q16FromFloat(f*f), square, "square")
+    assert.Equal(t, Q16FromFloat(f), square.Div(q), "div")
   }
 }
 
 func TestRotation(t *testing.T) {
   // Create vector to rotate.
-  vec1 := mgl32.Vec3{0, 0.832050, 0.554700}
-  vec2 := Vec3Q6FromFloat(0, 0.832050, 0.554700)
+  vec1 := mgl32.Vec3{0, 0.832, 0.554}
+  vec2 := Vec3Q16FromFloat(0, 0.832, 0.554)
 
   // Create rotation quaternion.
   inc1 := mgl32.Quat{0, mgl32.Vec3{0.07, 0, 0}}
   inc1.W = 1.0 - 0.5*(inc1.X()*inc1.X()+inc1.Y()*inc1.Y()+inc1.Z()*inc1.Z())
 
-  inc2 := QuatQ6{Q6FromInt32(0), Vec3Q6FromFloat(0.07, 0, 0)}
-  inc2.W = Q6FromInt32(1).Sub(Q6FromFloat(0.5).Mul(inc2.X().Mul(inc2.X()).Add(inc2.Y().Mul(inc2.Y())).Add(inc2.Z().Mul(inc2.Z()))))
+  inc2 := QuatQ16{Q16FromInt32(0), Vec3Q16FromFloat(0.07, 0, 0)}
+  inc2.W = Q16FromInt32(1).Sub(Q16FromFloat(0.5).Mul(inc2.X().Mul(inc2.X()).Add(inc2.Y().Mul(inc2.Y())).Add(inc2.Z().Mul(inc2.Z()))))
 
   rotation1 := mgl32.QuatIdent()
   rotation2 := QuatIdent()
@@ -44,7 +44,7 @@ func TestRotation(t *testing.T) {
     diffX := vec2.X.Float() - vec1.X()
     diffY := vec2.Y.Float() - vec1.Y()
     diffZ := vec2.Z.Float() - vec1.Z()
-    if diffX > 0.00001 || diffY > 0.00001 || diffZ > 0.00001 {
+    if diffX > 0.001 || diffY > 0.001 || diffZ > 0.001 {
       t.Errorf("difference too big:\nX1: %.8f\nX2: %.8f\nY1: %.8f\nY2: %.8f\nZ1: %.8f\nZ2: %.8f",
         vec1.X(), vec2.X.Float(),
         vec1.Y(), vec2.Y.Float(),
